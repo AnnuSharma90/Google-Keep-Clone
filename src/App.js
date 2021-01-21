954ad4263677ee2f5e6350a9bcd2b8200eb5792f
Annu <54701665+AnnuSharma90@users.noreply.github.com>
2021-01-21 23:29:06 +0530

    formatting done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, {useState} from 'react';
-import 'tachyons';
-import Header from './Header';
-import Footer from './Footer';
-import Createnote from './Createnote';
-import Note from './Note';
+import React, { useState } from "react";
+import "tachyons";
+import Header from "./Header";
+import Footer from "./Footer";
+import Createnote from "./Createnote";
+import Note from "./Note";
 
 function App() {
-  const[addItem, setAddItem]= useState([]);
+  const [addItem, setAddItem] = useState([]);
 
-  const addNote=(note)=>{
-    setAddItem((prevData)=>{
-      return[...prevData,note];
+  const addNote = (note) => {
+    setAddItem((prevData) => {
+      return [...prevData, note];
     });
-  }
+  };
 
-  const onDelete=(id)=>{
-    setAddItem((olddata)=>
-      olddata.filter((curdata, index)=>{
-      return index !== id;
-    })
-  );
+  const onDelete = (id) => {
+    setAddItem((olddata) =>
+      olddata.filter((curdata, index) => {
+        return index !== id;
+      })
+    );
   };
 
   return (
     <div>
-    <Header/>
-    <Createnote passNote={addNote}/>
-    {addItem.map((val, index)=>{
-      return(
-    <Note 
-        key={index}
-        id={index} 
-        title={val.title} 
-        content={val.content}
-        delItem={onDelete}/>
-      );
-    })}
-    <Footer/> 
+      <Header />
+      <Createnote passNote={addNote} />
+      {addItem.map((val, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={val.title}
+            content={val.content}
+            delItem={onDelete}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Footer=()=>{
+const Footer = () => {
 	const year = new Date().getFullYear();
 
-	return(
+	return (
 		<div className=" mw-100 pv4 ph3 ph4-m ph5-l mid-gray">
-		  <small className="f6 db tc">© {year} <b className="ttu">Google Keep Clone</b>., All Rights Reserved</small>
-		  <div className="tc mt3">
-		</div>
+			<small className="f6 db tc">
+				© {year} <b className="ttu">Google Keep Clone</b>., All Rights
+				Reserved
+			</small>
+			<div className="tc mt3"></div>
 		</div>
 	);
-
 };
 export default Footer;
